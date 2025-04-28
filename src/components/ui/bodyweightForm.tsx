4bffea320451c564186e4bc1018419fb97ee48c2
KeoniHan2024 <keonihan@gmail.com>
2025-04-27 22:37:16 -1000

    bodweight form now sends a post request to create bodyweight entry

--- src/components/ui/bodyweightForm.tsx
+++ src/components/ui/bodyweightForm.tsx
@@ -1,12 +1,57 @@
+import axios from "axios";
+import { FormEvent } from "../../types";
+
 const BodyWeightForm = () => {
+  const token = localStorage.getItem("token") as string;
   const date = new Date();
   const dd = String(date.getDate()).padStart(2, "0");
   const mm = String(date.getMonth() + 1).padStart(2, "0");
   const yyyy = date.getFullYear();
   const todayDate = yyyy + "-" + mm + "-" + dd;
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const API_URL_CREATE_BODYWEIGHT = import.meta.env.VITE_APP_API_URL?.concat(
+          "/bodyweights/create"
+        ) as string;
+        const formData = new FormData(event.target as HTMLFormElement);
+        var payload = Object.fromEntries(formData);
+        const date_worked = payload.date + " 00:00:00";
+      
+        axios
+        .post(
+          API_URL_CREATE_BODYWEIGHT,
+          {
+            payload,
+            date_worked
+          },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((response) => {
+          // setErrorMessage("");
+          // setsuccessMessage(response.data.message);
+          // setNewSets((prev) => prev + 1); // when an exercise is created successfully wit will then update the count which the useeffect is dependent on
+        })
+        .catch((err) => {
+          if (err.response) {
+            if (err.response.status === 400) {
+              // setsuccessMessage("");
+              // setErrorMessage(err.response.data.message);
+            }
+          }
+        });
+
+  }
+
+
+
   return (
     <>
       <div className="form-container">
+        <form className="form" onSubmit={handleSubmit}>
         <p className="title">Log Bodyweight</p>
         {/* {errorMessage && (
           <div className="alert alert-danger">{errorMessage}</div>
@@ -21,7 +66,7 @@ const BodyWeightForm = () => {
             name="bodyWeight"
             id="bodyWeightField"
             placeholder="Enter Weight"
-          />
+            />
         </div>
         <div className="input-group">
           <div className="form-column">
@@ -38,12 +83,13 @@ const BodyWeightForm = () => {
               name="date"
               id="dateField"
               defaultValue={todayDate}
-            />
+              />
           </div>
         </div>
         <button type="submit" className="sign">
           Enter weight
         </button>
+              </form>
       </div>
 
       {/* <div className="form-group" style={{ position: 'relative' }}>
