ee008b61a7e8d3c4b86317c419953ed2e2de4b09
KeoniHan2024 <keonihan@gmail.com>
2025-05-09 13:45:06 -1000

    delete all bodyweights works

--- src/components/ui/deleteSettingsContainer.tsx
+++ src/components/ui/deleteSettingsContainer.tsx
@@ -1,15 +1,17 @@
+interface deleteSetingsProps {
+  showDeleteBWModal: Boolean;
+  setShowDeleteBWModal: React.Dispatch<React.SetStateAction<Boolean>>;
+}
 
+function DeleteSettingsContainer({ props }: { props: deleteSetingsProps }) {
+  const handleBodyweightDeleteModal = () => {
+    props.setShowDeleteBWModal(!props.showDeleteBWModal);
+  };
 
-
-
-
-
-function DeleteSettingsContainer() {
-    
   return (
     <>
       <div className="settings-button-container">
-        <button>Delete all </button>
+        <button onClick={handleBodyweightDeleteModal}>Delete all </button>
         <button>Import Sets</button>
       </div>
     </>
--- src/pages/protected/Settings.tsx
+++ src/pages/protected/Settings.tsx
@@ -8,16 +8,29 @@ import axios from "axios";
 const API_IMPORT_BODYWEIGHTS = import.meta.env.VITE_APP_API_URL?.concat(
   "/bodyweights/import/"
 ) as string;
+const API_DELETE_BODYWEIGHTS = import.meta.env.VITE_APP_API_URL?.concat(
+  "/bodyweights/"
+) as string;
 
 function Settings() {
   const token = localStorage.getItem("token");
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [errorMessage, setErrorMessage] = useState<String>("");
+  const [successMessage, setSuccessMessage] = useState<String>("");
+
   //IMPORT BW MODAL
   const [showBWModal, setShowBWModal] = useState<Boolean>(false);
+  const [showDeleteBWModal, setShowDeleteBWModal] = useState<Boolean>(false);
 
   //IMPORT SETS MODAL
 
   //FORM handlers
+  const handleModalClose = () => {
+    setShowBWModal(false);
+    setShowDeleteBWModal(false);
+    setSuccessMessage("");
+    setErrorMessage("");
+  };
   // sends file to the backend to import all bodyweight and dates
   const handleImportBodyweight = (e: FormEvent) => {
     e.preventDefault();
@@ -34,27 +47,85 @@ function Settings() {
           },
         })
         .then((response) => {
-          console.log(response.data);
+          setErrorMessage("");
+          setSuccessMessage("Imported all bodyweights!");
         })
         .catch((err) => {
           if (err.response.data.message == "Token Expired") {
             localStorage.removeItem("token");
           }
+          setErrorMessage("Failed to import bodyweights :(");
+          setSuccessMessage("");
         });
     }
   };
 
+  const handleDeleteBodyweight = () => {
+    axios
+      .delete(API_DELETE_BODYWEIGHTS, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setErrorMessage("");
+        setSuccessMessage("Deleted all bodyweights");
+      })
+      .catch((err) => {
+        if (err.response.data.message == "Token Expired") {
+          localStorage.removeItem("token");
+        }
+        setErrorMessage("Failed to delete bodyweights");
+        setSuccessMessage("");
+      });
+  };
+
   const props = { showBWModal, setShowBWModal };
+  const deleteProps = { showDeleteBWModal, setShowDeleteBWModal };
   return (
     <>
       <Header showNav={true} textColor="white" loggedIn={true} />
       {props.showBWModal && (
         <div className="modal-div">
           <div className="form-container">
+            {errorMessage && (
+              <div className="alert alert-danger">{errorMessage}</div>
+            )}
+            {successMessage && (
+              <div className="alert alert-success">{successMessage}</div>
+            )}
+            <div className="modal-row exit-row">
+              <button onClick={handleModalClose}>X</button>
+            </div>
             <form id="import-bodyweight" onSubmit={handleImportBodyweight}>
-              <input id="file" type="file" accept=".csv" ref={fileInputRef} />
+              <div className="modal-row">
+                <input id="file" type="file" accept=".csv" ref={fileInputRef} />
+              </div>
               <button type="submit">Submit</button>
             </form>
+          </div>
+        </div>
+      )}
+
+      {deleteProps.showDeleteBWModal && (
+        <div className="modal-div">
+          <div className="form-container">
+            {errorMessage && (
+              <div className="alert alert-danger">{errorMessage}</div>
+            )}
+            {successMessage && (
+              <div className="alert alert-success">{successMessage}</div>
+            )}
+            <div className="modal-row exit-row">
+              <button onClick={handleModalClose}>X</button>
+            </div>
+            <div className="modal-row exit-row">
+              <p>
+                Are you sure you want to delete all bodyweights? This function
+                cannot be undone
+              </p>
+            </div>
+            <button onClick={handleDeleteBodyweight}>Confirm</button>
           </div>
         </div>
       )}
@@ -67,7 +138,7 @@ function Settings() {
           </div>
           <div className="settings-column">
             <p>Delete</p>
-            <DeleteSettingsContainer />
+            <DeleteSettingsContainer props={deleteProps} />
           </div>
         </div>
       </div>
